337a9ec93b9b71f1d9af18eb4caad960d3f52baf
1993833 <1993833@E20758.CEGEPGIM.LCL>
2019-07-11 14:43:12 -0400

    NETBEANGITHUB

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -16,7 +16,8 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        public static void main(string[] args)
+        System.out.println("this is small");
+        System.out.println("this is toolong")
     }
     
 }
